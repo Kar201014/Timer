d500c93e95db79b187c518201e7c109e5f63bf14
Kar201014 <karenkarapetian2@gmail.com>
2024-11-24 13:54:44 +0400

    Add files via upload

---
+++ stoopwatch.py
@@ -0,0 +1,23 @@
+# def start_stopwatch():
+#     start_time = time.time()
+#     input("Stopwatch started. Press Enter to stop.")
+#     end_time = time.time()
+#     elapsed_time = end_time - start_time
+#     print(elapsed_time)
+
+# print("Welcome to th1e Python Stopwatch!")
+
+# while True:
+#     print("\nOptions: ")
+#     print("1. Start Stopwatch")
+#     print("2. Exit")
+
+#     choice = input("Enter your choice (1-2): ")
+
+#     if choice == '1':
+#         print(f"Elapsed Time: {start_stopwatch()}")
+#     elif choice == '2':
+#         print("Exiting the stopwatch. Goodbye!")
+#         break
+#     else:
+#         print("Invalid choice. Please enter 1 or 2.")
